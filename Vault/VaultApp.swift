d50fd6ebe1ee122785fcb1080764706c7cc53b3e
Dan <Luthor>
2026-03-24 10:27:39 +0800

    Initial Commit

---
+++ Vault/VaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  VaultApp.swift
+//  Vault
+//
+//  Created by Dan on 2026/3/24.
+//
+
+import SwiftUI
+
+@main
+struct VaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
